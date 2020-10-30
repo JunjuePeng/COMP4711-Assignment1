0447b283db91bb73a96dc9f826b88b9bdb28310c
JunjuePeng <1798751445@qq.com>
2020-10-30 20:20:28 +0800

    Add some page

---
+++ app/Views/oneplace.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>Player details</h1>
+		<p><strong>ID:</strong> {id}</p>
+		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Contry:</strong> {contry}</p>
+		<p><strong>Height:</strong> {height}</p>
+		<p><strong>Weight:</strong> {weight}</p>
+                <p><strong>Native-place:</strong> {native place}</p>
+                <p><strong>Age:</strong> {age}</p>
+                 <p><strong>Graduate School:</strong> {graduate school}</p>
+                
+		<p><img src="/image/{image}"/></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
